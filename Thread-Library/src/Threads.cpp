c70ea63e037e2f15ccba79c99cbea629eb868abb
sumanth <sumanth@iastate.edu>
2019-12-04 14:31:05 -0600

    Missed some files.

--- Thread-Library/src/Threads.cpp
+++ Thread-Library/src/Threads.cpp
@@ -1,7 +1,12 @@
 #include "Threads.h"
 
 Threads::Threads(/* args */) {
-    
+    //@TODO determine the number of threads to create using the data from ML.
+    //Creating 3 threads for now.
+    for(int i = 0; i < 3; i++)
+    {
+        //Thread 
+    }
 }
 
 Threads::~Threads() {
@@ -11,5 +16,5 @@ Threads::~Threads() {
 template<typename T>
 bool Threads::addProcess(T* funcPtr)
 {
-    
+    // based on the ML values assign this function to a thread in the Map
 }
--- Thread-Library/tests/main.cpp
+++ Thread-Library/tests/main.cpp
@@ -1,7 +1,14 @@
 #include "include/PoolManager.h"
+#include <iostream>
 
+void print()
+{
+    std::cout<< "Hello World";
+}
 
 int main(int argc, char* argv[])
 {
-    PoolM
+    std::unique_ptr<PoolManager> plManager = std::make_unique<PoolManager>(plManager->getInstance());
+    plManager->AddProcess(&print);
+
 }
